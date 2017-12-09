3dc870abe015af5f8d863f71439a5d5908523d56
DyonR <DyonR@users.noreply.github.com>
2017-12-09 20:20:39 +0100

    Added a try-catch to the checkRSS

--- HardstylePodcastNotifierBot.ps1
+++ HardstylePodcastNotifierBot.ps1
@@ -53,64 +53,68 @@ function CheckRSS{
         [Parameter(Mandatory=$true)][string]$LastReleaseTitle,
         [Parameter(Mandatory=$true)][string]$rssUrl
     )
+    try{
+        $rssResponse = [xml](Invoke-WebRequest $rssUrl)
+        if($ShortName -eq "KTRA"){
+            $NewReleaseTitle = ($rssResponse.rss.channel.item | Select-Object -Last 1).title
+            $DownloadUrl = ($rssResponse.rss.channel.item | Select-Object -Last 1).origEnclosureLink
+        }
+        else{
+            $NewReleaseTitle = $rssResponse.rss.channel.item[0].title
+            $DownloadUrl = $rssResponse.rss.channel.item[0].enclosure.url
+        }
 
-    $rssResponse = [xml](Invoke-WebRequest $rssUrl)
-    if($ShortName -eq "KTRA"){
-        $NewReleaseTitle = ($rssResponse.rss.channel.item | Select-Object -Last 1).title
-        $DownloadUrl = ($rssResponse.rss.channel.item | Select-Object -Last 1).origEnclosureLink
-    }
-    else{
-        $NewReleaseTitle = $rssResponse.rss.channel.item[0].title
-        $DownloadUrl = $rssResponse.rss.channel.item[0].enclosure.url
-    }
+        if($NewReleaseTitle.Count -ne 1){$NewReleaseTitle = $NewReleaseTitle[0]}
 
-    if($NewReleaseTitle.Count -ne 1){$NewReleaseTitle = $NewReleaseTitle[0]}
+        if($DownloadUrl.Contains('?')){
+            $DownloadUrl = $DownloadUrl.Substring(0,$DownloadUrl.IndexOf('?'))
+        }
 
-    if($DownloadUrl.Contains('?')){
-        $DownloadUrl = $DownloadUrl.Substring(0,$DownloadUrl.IndexOf('?'))
-    }
-
-    if($NewReleaseTitle -ne $LastReleaseTitle){
-        $DownloadUrlTelegram = $DownloadUrl
-        $NewReleaseTitleTelegram = $NewReleaseTitle
-        if($DownloadUrlTelegram.Contains('#')){
-            $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\#', '%23'
-        }
-        if($NewReleaseTitleTelegram.Contains('#')){
-            $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\#', '%23'
-        }
-        if($DownloadUrlTelegram.Contains('(')){
-            $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\(', '%28'
-        }
-        if($NewReleaseTitleTelegram.Contains('(')){
-            $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\(', '%28'
-        }
-        if($DownloadUrlTelegram.Contains(')')){
-            $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\)', '%29'
-        }
-        if($NewReleaseTitleTelegram.Contains(')')){
-            $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\)', '%29'
-        }
-        if($NewReleaseTitleTelegram.Contains('&')){
-            $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\&', '%26'
-        }
-        try{
-        $TelegramResponse = Invoke-WebRequest -Uri "https://api.telegram.org/bot$TelegramBotToken/sendMessage?chat_id=$TelegramChatId&parse_mode=markdown&text=[$NewReleaseTitleTelegram]($DownloadUrlTelegram)" -Method POST
-            if($TelegramResponse.StatusCode -eq "200"){
-				Write-Host "New $ShortName! Message has successfully been sent."
-                $PodcastInfo.$ShortName.ChildNodes.Item(7).'#text' = $NewReleaseTitle
-                $PodcastInfo.Save("$ScriptPath\PodcastsConfig\" + $ShortName + ".xml")
+        if($NewReleaseTitle -ne $LastReleaseTitle){
+            $DownloadUrlTelegram = $DownloadUrl
+            $NewReleaseTitleTelegram = $NewReleaseTitle
+            if($DownloadUrlTelegram.Contains('#')){
+                $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\#', '%23'
+            }
+            if($NewReleaseTitleTelegram.Contains('#')){
+                $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\#', '%23'
+            }
+            if($DownloadUrlTelegram.Contains('(')){
+                $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\(', '%28'
+            }
+            if($NewReleaseTitleTelegram.Contains('(')){
+                $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\(', '%28'
+            }
+            if($DownloadUrlTelegram.Contains(')')){
+                $DownloadUrlTelegram = $DownloadUrlTelegram -Replace '\)', '%29'
+            }
+            if($NewReleaseTitleTelegram.Contains(')')){
+                $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\)', '%29'
+            }
+            if($NewReleaseTitleTelegram.Contains('&')){
+                $NewReleaseTitleTelegram = $NewReleaseTitleTelegram -Replace '\&', '%26'
+            }
+            try{
+            $TelegramResponse = Invoke-WebRequest -Uri "https://api.telegram.org/bot$TelegramBotToken/sendMessage?chat_id=$TelegramChatId&parse_mode=markdown&text=[$NewReleaseTitleTelegram]($DownloadUrlTelegram)" -Method POST
+                if($TelegramResponse.StatusCode -eq "200"){
+				    Write-Host "New $ShortName! Message has successfully been sent."
+                    $PodcastInfo.$ShortName.ChildNodes.Item(7).'#text' = $NewReleaseTitle
+                    $PodcastInfo.Save("$ScriptPath\PodcastsConfig\" + $ShortName + ".xml")
+		        }
+            }
+            catch{
+                if($_.Exception.InnerException.Response.StatusCode.value__ -ne "200"){
+	                Write-Host "An error occured when trying to send a message to Telegram."
+                }
 		    }
         }
-        catch{
-            if($_.Exception.InnerException.Response.StatusCode.value__ -ne "200"){
-			Write-Host "An error occured when trying to send a message to Telegram."
-            }
-        }
+        #else{
+        #	Write-Host "A new $ShortName is not yet online." 
+        #}
     }
-	#else{
-	#	Write-Host "A new #shortname is not yet online." 
-	#}
+	catch{
+		Write-Host "Error obtaining the RSS feed." 
+	}
 }
 
 while($true){
